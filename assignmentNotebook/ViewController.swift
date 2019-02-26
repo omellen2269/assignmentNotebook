15a432b82b47006b205f249162cd638baaa09431
Olivia Mellen <omellen2269@jh-mba158-18.local>
2019-02-26 14:24:35 -0600

    Initial Commit

---
+++ assignmentNotebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignmentNotebook
+//
+//  Created by Olivia Mellen on 2/26/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
